eeb2524df8a4b1d3dfca676625ec8f2421f95924
LordSnow <jeantshimbalanga9@gmail.com>
2024-12-17 10:28:30 +0100

    Done with login and register

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,16 @@
 <?php
 
+// use App\Http\Controllers\Auth\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticationController;
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::post("/register", [AuthenticationController::class, 'register']);
+Route::post("/login", [AuthenticationController::class, 'login']);
 
-Route::get("/test", function(){
-    return response([
-        "message" => "This is a test route."
-    ], 200);
-});
+
